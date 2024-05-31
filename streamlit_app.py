d25928b176ddaea566a5f15bbea5a9495e314f29
colorfulcereal <hamsini.m.ramanathan@gmail.com>
2024-05-31 03:12:32 +0000

    Adding matplotlib

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,39 +2,19 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+columns = ['Overall Qual', 'SalePrice']
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+df = pd.read_csv('http://jse.amstat.org/v19n3/decock/AmesHousing.txt', usecols=columns, sep='\t')
+columns = ['Overall Qual', 'SalePrice']
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+df.plot('Overall Qual', 'SalePrice')
+plt.title("Comparison of how Quality affects the Sale Price")
+plt.xlabel("Overall Quality")
+plt.ylabel("Sale Price")
+plt.xlim(0, 10.5)
+plt.ylim(0,800000.5)
+plt.show()
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
